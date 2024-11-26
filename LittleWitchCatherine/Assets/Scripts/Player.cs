3991f14d05cfa810122394ded374cc666f4e09d3
DeDMaKSiM1 <maxlex2012@gmail.com>
2024-11-26 13:17:54 +0300

    Temp в процессе реализации движения

---
+++ LittleWitchCatherine/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float Speed = 1f;
+    private Rigidbody2D rbody;
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        if()
+        var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Debug.Log(input);
+        //rbody.AddForce(input);
+        rbody.MovePosition(input);
+    }
+}
