952629df0c113f2f8fcff60d556f28ec1f263e50
DeDMaKSiM1 <maxlex2012@gmail.com>
2024-11-28 08:50:21 +0300

    Анимации бега и idle сделаны, blend-tree реализован

--- LittleWitchCatherine/Assets/Scripts/Player.cs
+++ LittleWitchCatherine/Assets/Scripts/Player.cs
@@ -6,20 +6,40 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rbody;
     private Vector2 movement;
+    private Animator animator;
+
+    private readonly string HorizontalMovingValue = "HorizontalValue";
+    private readonly string VerticleMovingValue = "VerticleValue";
+    private readonly string IsRunning = "IsRunning";
     private void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     private void Update()
     {
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Debug.Log(movement);
     }
     private void FixedUpdate()
-    {        
-        if (movement.magnitude > 1)
-            movement.Normalize();
-        rbody.MovePosition(rbody.position + Speed * Time.fixedDeltaTime * movement);
+    {       
+        //idle
+        if(movement == Vector2.zero)
+        {
+            animator.SetBool(IsRunning, false);
+        }
+        else
+        {
+            if (movement.magnitude > 1)
+                movement.Normalize();
+            animator.SetBool(IsRunning, true);
+            animator.SetFloat(HorizontalMovingValue, movement.x);
+            animator.SetFloat(VerticleMovingValue, movement.y);
+            Debug.Log(movement);
+            rbody.MovePosition(rbody.position + Speed * Time.fixedDeltaTime * movement);
+        }
+        
+       
+
 
     }
 }
