d7551606d5e70901f595dd32f6093cf368ff2153
DeDMaKSiM1 <maxlex2012@gmail.com>
2024-11-28 09:19:01 +0300

    Реализован бег + анимации бега

--- LittleWitchCatherine/Assets/Scripts/Player.cs
+++ LittleWitchCatherine/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float Speed = 1f;
+    [SerializeField] private float BaseSpeed = 1f;
 
     private Rigidbody2D rbody;
     private Vector2 movement;
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
 
     private readonly string HorizontalMovingValue = "HorizontalValue";
     private readonly string VerticleMovingValue = "VerticleValue";
+    private readonly string IsWalking = "IsWalking";
     private readonly string IsRunning = "IsRunning";
     private void Start()
     {
@@ -21,25 +22,45 @@ public class Player : MonoBehaviour
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
     private void FixedUpdate()
-    {       
-        //idle
-        if(movement == Vector2.zero)
+    {
+        //Покой
+        if (movement == Vector2.zero)
         {
+            animator.SetBool(IsWalking, false);
             animator.SetBool(IsRunning, false);
         }
+        //Движение
         else
         {
+            //Нормализация вектора движения
             if (movement.magnitude > 1)
                 movement.Normalize();
-            animator.SetBool(IsRunning, true);
+
+            //Проверка на движение
+            animator.SetBool(IsWalking, true);
+            //Проверка условия на бег
+            var speed = BaseSpeed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                animator.SetBool(IsRunning, true);
+                speed *= 2;
+            }
+            else
+            {
+                animator.SetBool(IsRunning, false);                
+            }
+
+
+            //Установка значения движения для анимаций движения
             animator.SetFloat(HorizontalMovingValue, movement.x);
             animator.SetFloat(VerticleMovingValue, movement.y);
-            Debug.Log(movement);
-            rbody.MovePosition(rbody.position + Speed * Time.fixedDeltaTime * movement);
-        }
-        
-       
 
+            rbody.MovePosition(rbody.position + speed * Time.fixedDeltaTime * movement);
+        }
+    }
+
+    private void SetAnimatorConditions()
+    {
 
     }
 }
