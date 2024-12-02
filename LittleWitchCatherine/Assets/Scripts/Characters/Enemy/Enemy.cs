4c8919c45699d2b1d3e15c0cf002870bd0ba0866
DeDMaKSiM1 <maxlex2012@gmail.com>
2024-12-02 08:44:58 +0300

    Сделал границы коллайдера для перемещений противника

--- LittleWitchCatherine/Assets/Scripts/Characters/Enemy/Enemy.cs
+++ LittleWitchCatherine/Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Collider2D Border;
+
     private float BaseSpeed = 0.25f;
     private Rigidbody2D rbody;
     private Animator animator;
     private Vector2 movement;
-
+    private Vector2 minBounds;
+    private Vector2 maxBounds;
 
     private void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
         StartCoroutine(MovementCoroutine());
     }
 
@@ -21,14 +25,28 @@ public class Enemy : MonoBehaviour
     {
         while (true)
         {
-            int directionX = Random.Range(-1, 2);
-            int directiony = Random.Range(-1, 2);
+            int directionX, directiony;
+
+            if (!IsWithinBounds(transform, Border))
+            {
+                directionX = (int)(Border.transform.position.x - transform.position.x);
+                directiony = (int)(Border.transform.position.y - transform.position.y);
+            }
+            else
+            {
+                directionX = Random.Range(-1, 2);
+                directiony = Random.Range(-1, 2);
+            }
+
             movement = new Vector2(directionX, directiony);
             rbody.MovePosition(rbody.position + movement * BaseSpeed);
-            Debug.Log(movement * BaseSpeed);
-
             yield return new WaitForSeconds(1f);
+
         }
     }
-
+    private bool IsWithinBounds(Transform target, Collider2D boundary)
+    {
+        Bounds bounds = boundary.bounds;
+        return bounds.Contains(target.position);
+    }
 }
