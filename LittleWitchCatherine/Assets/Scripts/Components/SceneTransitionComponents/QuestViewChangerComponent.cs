f62a2f1b3351b3e9b6552fa064b10465d94b1e74
DeDMaKSiM1 <maxlex2012@gmail.com>
2024-12-13 19:54:10 +0300

    в непонятнках с квестовым видом

--- LittleWitchCatherine/Assets/Scripts/Components/SceneTransitionComponents/QuestViewChangerComponent.cs
+++ LittleWitchCatherine/Assets/Scripts/Components/SceneTransitionComponents/QuestViewChangerComponent.cs
@@ -5,7 +5,7 @@ public class QuestViewChangerComponent : MonoBehaviour
     void Start()
     {
         var initialSceneName = PlayerPrefs.GetString("LocationName");
-        var locationPrefab = Resources.Load(initialSceneName) as GameObject ;
+        var locationPrefab = Resources.Load($"QuestViewSprites/{initialSceneName}") as GameObject ;
         Instantiate(locationPrefab);
     }
 
