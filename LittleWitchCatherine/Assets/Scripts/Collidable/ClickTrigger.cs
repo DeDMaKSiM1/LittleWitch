1a260cfc5ad5a2fda67caa9d26048d1faa6faccb
DeDMaKSiM1 <maxlex2012@gmail.com>
2024-12-19 19:53:39 +0300

    Сделал смену курсоров

--- LittleWitchCatherine/Assets/Scripts/Collidable/ClickTrigger.cs
+++ LittleWitchCatherine/Assets/Scripts/Collidable/ClickTrigger.cs
@@ -4,8 +4,19 @@ using UnityEngine.Events;
 public class ClickTrigger : MonoBehaviour
 {
     public UnityEvent OnClick;
+    public UnityEvent OnEnter;
+    public UnityEvent OnExit;
+
     private void OnMouseDown()
     {
         OnClick.Invoke();
+    }
+    private void OnMouseExit()
+    {
+        OnExit.Invoke();
+    }
+    private void OnMouseEnter()
+    {
+        OnEnter.Invoke();
     }
 }
